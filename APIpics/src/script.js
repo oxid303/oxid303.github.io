0a854cde377169892f1cfce8d72575f599f2a618
Ann <oxid303@gmail.com>
2019-05-11 11:06:59 +0300

    change API without slick

--- APIpics/src/script.js
+++ APIpics/src/script.js
@@ -1,57 +1,105 @@
 $(() => {
-  const xApiKey = "7617f81f-b406-4498-83d2-9af7c0c4138b";
-  const url = 'https://api.thedogapi.com/v1/images/search';
+  // var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
 
-  let autoplay = false,
+  let history = [],
+    urlImage,
+    urlImagePrev,
+    urlImageModalOpen,
+    newImageDate,
+    lastImageDate = 0,
+    newArrowDate,
+    lastArrowDate = 0,
+    width = $(window).width(),
+    picsAmount = getPicsWidth(),
+    modal = false,
+    autoplay = false,
     seconds = 5,
     run;
 
-  function viewImage(urlImage) {
-    $('#my-image').attr('src', urlImage);
+  function getPicsWidth() {
+    return (width < 480) ? 4 :
+      (width < 624) ? 6 :
+        (width < 776) ? 8 :
+          (width < 992) ? 10 :
+            9;
+  };
+
+  $(window).resize(() => {
+    width = $(window).width();
+    let picsAmountTmp = getPicsWidth();
+
+    if (picsAmount != picsAmountTmp) {
+      picsAmount = picsAmountTmp;
+      viewImages();
+    }
+  });
+
+  function viewBigImage(id, url) {
+    urlImageModalOpen = url;
+    $('.big-image').attr({ id: id, src: url });
+    $('#caption').attr('href', url);
 
     const image = new Image();
-    image.src = urlImage;
+    image.src = url;
     image.onload = function () {
-      $('#image-attribute').text(`Pixels: ${this.width} x ${this.height}`);
+      $('#caption').text(`Open (${this.width} x ${this.height})`);
     }
   }
 
+  function viewImages(idStart, idEnd, idCurr) {
+    $('.images img, .images button').remove();
+
+    for (let i = idStart; i < idEnd; i++) {
+      if (i < 0) continue;
+      $('.images').append(`<img id="${history[i].id}" src="${history[i].src}" alt="default">`);
+      if (i == idCurr) {
+        $(`#id${i}`).addClass('highlight').removeClass('no-highlight');
+      } else {
+        $(`#id${i}`).removeClass('highlight').addClass('no-highlight');
+      }
+    }
+
+    if (picsAmount == 9) $('.images').append(`<button type="button" class="btn btn-raised btn-info">&#10154;</button>`);
+  }
+
   function getNewImage() {
-    const request = $.ajax(url, {
-      accepts: {
-        'x-api-key': xApiKey,
-        'Content-Type': 'application/json',
-      },
-    });
+    newImageDate = new Date().getTime();
+    if (lastImageDate + 1499 > newImageDate) return;
+    lastImageDate = newImageDate;
 
-    $('#my-image').attr('src', './img/default.gif');
+    $('.big-image-container').html('<img class="loader" src="./img/loader.gif" alt="pic">');
 
-    request
-      .then((results) => {
+    fetch('https://source.unsplash.com/random')
+      .then((responce) => {
+        urlImage = responce.url;
+        if (urlImage == urlImagePrev) {
+          $('.loader').attr({ src: urlImagePrev, class: 'big-image' });
+          return;
+        };
+        urlImagePrev = urlImage;
 
-        const firstItem = results[0];
-        const { url: urlImage } = firstItem;
+        history.push({ id: 'id' + history.length, src: urlImage })
 
-        $('#my-image').remove();
-        $('.image-content').html('<img src="" id="my-image" class="image" alt="pic" />');
+        $('.big-image').remove();
+        $('.big-image-container').html(`<img class="big-image" alt="pic">`);
 
-        viewImage(urlImage);
+        const idImage = `big${history.length - 1}`
+        viewBigImage(idImage, urlImage);
 
-        $('#images button').remove();
-
-        $('#images').append(`<img src="${urlImage}" alt="default" />`);
-        if ($('#images img').length > 9) {
-          $('#images img:first').remove();
+        let idImagesFirst = history.length - picsAmount,
+          idImagesLast = idImagesFirst + picsAmount;
+        if (idImagesFirst < 0) {
+          idImagesLast = picsAmount - Math.abs(idImagesFirst);
+          idImagesFirst = 0;
         }
-
-        $('#images').append(`<button class="button-new-image">&#10154;</button>`);
+        viewImages(idImagesFirst, idImagesLast, history.length - 1);
       })
       .catch((err) => {
-        console.log('Error555:', err);
+        console.log('Error555: ', err);
       });
   }
 
-  $('#images').append(`<button class="button-new-image">&#10154;</button>`);
+  if (picsAmount == 9) $('.images').append(`<button type="button" class="btn btn-raised btn-info">&#10154;</button>`);
 
   getNewImage();
 
@@ -60,22 +108,35 @@ $(() => {
     if (autoplay) runSetInterval();
   });
 
-  $('.image-content').on('click', 'img', () => {
-    $('#image-modal')[0].src = $('#my-image')[0].src;
+  $('.big-image-container').on('click', 'img', () => {
+    modal = true;
+    $('#image-modal')[0].src = $('.big-image')[0].src;
     $('#my-modal').css('display', 'block');
-    // $('#caption').html($('#my-image')[0].alt);
   });
 
   $('.close').click(() => {
+    modal = false;
     $('#my-modal').css('display', 'none');
   });
 
-  $('#images').on('click', 'img', function() {
-    let urlImage = this.src;
-    viewImage(urlImage);
+  $('#caption').mousedown((e) => {
+    if (e.which == 1) {
+      window.open(urlImageModalOpen, '_blank');
+    };
   });
 
-  $('#images').on('click', 'button', function() {
+  $('.images').on('click', 'img', function () {
+    const urlImageStr = this.src,
+      idImageStr = `big${+this.id.substring(2)}`;
+    viewBigImage(idImageStr, urlImageStr);
+
+    const idImagesFirst = +$('.images img:first')[0].id.substring(2),
+      idImagesLast = +$('.images img:last')[0].id.substring(2),
+      idImageTmp = +this.id.substring(2);
+    viewImages(idImagesFirst, idImagesLast + 1, idImageTmp);
+  });
+
+  $('.images').on('click', 'button', function () {
     getNewImage();
     if (autoplay) runSetInterval();
   });
@@ -114,6 +175,8 @@ $(() => {
         if (seconds > 1 && seconds <= 10) seconds -= 1;
         if (seconds > 10) seconds -= 5;
         break;
+      default:
+        break;
     }
     $('#timeset').html(`<p>${seconds} seconds</p>`);
     if (autoplay) runSetInterval();
@@ -123,33 +186,53 @@ $(() => {
   $('#time-down').click(() => timeSet('down'));
 
   function arrow(key) {
-    const urlImageNow = $('#my-image')[0].src;
-    const allImg = $('#images img');
+    const idImageBig = $('.big-image')[0].id ? +$('.big-image')[0].id.substring(3) : history.length - 1,
+      idImageFirst = +$('.images img:first')[0].id.substring(2),
+      idImageLast = +$('.images img:last')[0].id.substring(2) + 1;
 
-    for (let i = allImg.length - 1; i >= 0; i--) {
-      const urlTmp = allImg[i].src;
+    switch (key) {
+      case 'next':
+        if (idImageBig == history.length - 1) {
+          getNewImage();
+          break;
+        };
+        const idNext = `big${idImageBig + 1}`,
+          urlNext = history[idImageBig + 1].src;
+        viewBigImage(idNext, urlNext);
 
-      if (urlTmp == urlImageNow) {
-        if (key == 'next') {
-          if (i == allImg.length - 1) {
-            getNewImage();
-            break;
-          }
-          const urlNext = allImg[i + 1].src;
-          viewImage(urlNext);
-          break;
-        }
-        if (key == 'prev') {
-          if (i == 0) break;
-          const urlPrev = allImg[i - 1].src;
-          viewImage(urlPrev);
-          break;
-        }
-      }
+        if (idImageBig == idImageLast - 1) {
+          viewImages(idImageFirst + 1, idImageLast + 1, idImageBig + 1);
+        } else {
+          viewImages(idImageFirst, idImageLast, idImageBig + 1);
+        };
+        break;
+
+      case 'prev':
+        if (idImageBig == 0) break;
+        const idPrev = `big${idImageBig - 1}`,
+          urlPrev = history[idImageBig - 1].src;
+        viewBigImage(idPrev, urlPrev);
+
+        if (history.length > picsAmount && idImageFirst == idImageBig) {
+          viewImages(idImageFirst - 1, idImageLast - 1, idImageBig - 1);
+        } else {
+          viewImages(idImageFirst, idImageLast, idImageBig - 1);
+        };
+        break;
+
+      default:
+        break;
     }
   }
 
+  $('.button-next').click(() => { arrow('next') });
+  $('.button-prev').click(() => { arrow('prev') });
+
   $(document).keydown((e) => {
+    newArrowDate = new Date().getTime();
+    if (modal || lastArrowDate + 100 > newArrowDate) return;
+    lastArrowDate = newArrowDate;
+
     if (e.keyCode == 39 || e.keyCode == 40) arrow('next');
     if (e.keyCode == 37 || e.keyCode == 38) arrow('prev');
   });
