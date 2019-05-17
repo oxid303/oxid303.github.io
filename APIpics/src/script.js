cec8f0fd9ad66ec836b9aad061cf1d406e569efc
Ann <oxid303@gmail.com>
2019-05-18 01:22:45 +0300

    test touch v2

--- APIpics/src/script.js
+++ APIpics/src/script.js
@@ -9,6 +9,7 @@ $(() => {
     newArrowDate,
     lastArrowDate = 0,
     width = $(window).width(),
+    height = $(window).height(),
     picsAmount = getPicsAmount(),
     modal = false,
     autoplay = false,
@@ -16,8 +17,9 @@ $(() => {
     run,
     mobile = false;
 
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|SymbianOS|Opera Mini/i.test(navigator.userAgent)) {
     mobile = true;
+    $('.side-button').css('display', 'none');
   }
 
   function getPicsAmount() {
@@ -29,8 +31,12 @@ $(() => {
 
   $(window).resize(() => {
     width = $(window).width();
-    let picsAmountTmp = getPicsAmount();
+    height = $(window).height();
+    // $('html').css('width', width + 'px');
 
+    if (mobile) viewMobile();
+
+    let picsAmountTmp = getPicsAmount();
     if (picsAmount != picsAmountTmp) {
       picsAmount = picsAmountTmp;
 
@@ -43,8 +49,37 @@ $(() => {
     }
   });
 
+  function viewMobile() {
+    if (width > height) {
+      $('.images-container').css('display', 'none');
+      $('.big-image').css({
+        'max-height': height - 40 + 'px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,-50%)',
+      });
+    } else {
+      $('.images-container').css('display', 'flex');
+      $('.big-image').css({
+        'max-height': height - 125 + 'px',
+        position: 'absolute',
+        top: '105px',
+        left: '50%',
+        transform: 'translate(-50%,0%)',
+      });
+
+    };
+  }
+
   function viewBigImage(id, url) {
+    if (mobile) viewMobile();
     urlModal = url;
+    if ($('.loader')[0]) {
+      $('.loader').addClass('big-image');
+      $('.loader').removeClass('loader');
+    };
+
     $('.big-image').attr({ id: id, src: url });
     $('#caption').attr('href', url);
 
@@ -62,14 +97,20 @@ $(() => {
     if (idLast > history.length) idLast = history.length;
 
     for (let i = idFirst; i < idLast; i++) {
-      $('.images').append(`<img id="${history[i].id}" src="${history[i].src}" alt="default">`);
+      $('.images').append(`<img id="${history[i].id}" src="${history[i].src}" class="images-item" alt="default">`);
       if (i == idCurr) {
         $(`#id${i}`).addClass('highlight').removeClass('no-highlight');
       } else {
         $(`#id${i}`).removeClass('highlight').addClass('no-highlight');
       }
     }
-    $('.images').append(`<button>&#10154;</button>`);
+
+    // '&#10153;' '&#10154;'
+    let buttonSymbol;
+    if (idCurr == history.length - 1) {
+      buttonSymbol = '&#10146;'
+    } else buttonSymbol = '&#10147;';
+    $('.images').append(`<button class="images-item">${buttonSymbol}</button>`);
   }
 
   function getNewImage() {
@@ -88,7 +129,7 @@ $(() => {
         };
         urlImagePrev = urlImage;
 
-        history.push({ id: 'id' + history.length, src: urlImage })
+        history.push({ id: 'id' + history.length, src: urlImage });
 
         $('.big-image').remove();
         $('.big-image-container').html(`<img class="big-image" alt="pic">`);
@@ -114,11 +155,7 @@ $(() => {
   });
 
   $('.big-image-container').on('click', 'img', () => {
-    // $('#caption').css({ ['display']: 'none' });
-    // $('.modal').css({ ['padding-top']: '10px' });
-    // $('.modal-content').css({ ['max-width']: '95%', ['max-height']: '95%' });
     if (mobile) return;
-    return;
     modal = true;
     $('#image-modal')[0].src = $('.big-image')[0].src;
     $('#my-modal').css('display', 'block');
@@ -195,7 +232,7 @@ $(() => {
   $('#time-down').click(() => timeSet('down'));
 
   function arrow(key) {
-    const idFirst = +$('.images img:first')[0].id.substring(2);
+    let idFirst = +$('.images img:first')[0].id.substring(2);
 
     switch (key) {
       case 'next':
@@ -216,7 +253,11 @@ $(() => {
         break;
 
       case 'prev':
-        if (idCurr == 0) break;
+        if (idCurr == 0) {
+          idCurr = history.length;
+          idFirst = history.length - picsAmount;
+          if (idFirst < 0) idFirst = 0;
+        }
         idCurr -= 1;
         const idPrev = `big${idCurr}`,
           urlPrev = history[idCurr].src;
@@ -227,6 +268,49 @@ $(() => {
         } else {
           viewImages(idFirst);
         };
+        break;
+
+      case 'scrollNext':
+        let idLast = +$('.images img:last')[0].id.substring(2);
+        if (idCurr == history.length - 1) {
+          getNewImage();
+          break;
+        }
+        if (idLast == history.length - 1) {
+          idCurr = history.length - 1;
+          const idBig = `big${idCurr}`,
+            urlBig = history[idCurr].src;
+          viewBigImage(idBig, urlBig);
+          viewImages(idFirst);
+        } else {
+          idCurr = idLast = idLast + picsAmount - 1;
+          if (idCurr > history.length - 1) idCurr = idLast = history.length - 1;
+          const idBig = `big${idCurr}`,
+            urlBig = history[idCurr].src;
+          viewBigImage(idBig, urlBig);
+          idFirst = idLast - picsAmount + 1;
+          if (idFirst < 0) idFirst = 0;
+          viewImages(idFirst);
+        }
+        break;
+
+      case 'scrollPrev':
+        if (idCurr == 0) break;
+        if (idFirst == 0) {
+          idCurr = 0;
+          const idBig = `big${idCurr}`,
+            urlBig = history[idCurr].src;
+          viewBigImage(idBig, urlBig);
+          viewImages(idFirst);
+          break;
+        } else {
+          idCurr = idFirst = idFirst - picsAmount + 1;
+          if (idCurr < 0) idCurr = idFirst = 0;
+          const idBig = `big${idCurr}`,
+            urlBig = history[idCurr].src;
+          viewBigImage(idBig, urlBig);
+          viewImages(idFirst);
+        }
         break;
 
       default:
@@ -242,8 +326,10 @@ $(() => {
     if (modal || lastArrowDate + 100 > newArrowDate) return;
     lastArrowDate = newArrowDate;
 
-    if (e.keyCode == 39 || e.keyCode == 40) arrow('next');
-    if (e.keyCode == 37 || e.keyCode == 38) arrow('prev');
+    if (e.keyCode == 39) arrow('next');
+    if (e.keyCode == 37) arrow('prev');
+    if (e.keyCode == 40) arrow('scrollNext');
+    if (e.keyCode == 38) arrow('scrollPrev');
   });
 
 
@@ -251,202 +337,109 @@ $(() => {
 
 
 
+  if (mobile) {
+    function swipe(el, settings) {
 
+      settings = Object.assign({}, {
+        minDist: 60,
+        maxDist: 200,
+        maxTime: 2000,
+        minTime: 50,
+      }, settings);
 
+      let dir,
+        swipeType,
+        dist,
+        isMouse = false,
+        isMouseDown = false,
+        startX = 0,
+        distX = 0,
+        startY = 0,
+        distY = 0,
+        startTime = 0,
+        touchElement;
 
-  /**
- * Функция определения события swipe на элементе.
- * param {Object} el - элемент DOM.
- * param {Object} settings - объект с предварительными настройками.
- */
-  var swipe = function (el, settings) {
+      function checkStart(e) {
+        let event = e.changedTouches[0];
+        if (typeof e.touches !== "undefined" && e.touches.length !== 1) return; // ignoring touch with a few fingers
+        dir = "none";
+        swipeType = "none";
+        dist = 0;
+        startX = event.pageX;
+        startY = event.pageY;
+        startTime = new Date().getTime();
+        isMouseDown = true;
+        // e.preventDefault();
 
-    // настройки по умолчанию
-    var settings = Object.assign({}, {
-      minDist: 60,
-      maxDist: 200,
-      maxTime: 2000,
-      minTime: 50
-    }, settings);
-
-    // коррекция времени при ошибочных значениях
-    if (settings.maxTime < settings.minTime) settings.maxTime = settings.minTime + 500;
-    if (settings.maxTime < 100 || settings.minTime < 50) {
-      settings.maxTime = 700;
-      settings.minTime = 50;
-    }
-
-
-
-    var dir,                  // направление свайпа (horizontal, vertical)
-      swipeType,            // тип свайпа (up, down, left, right)
-      dist,                 // дистанция, пройденная указателем
-      isMouse = false,      // поддержка мыши (не используется для тач-событий)
-      isMouseDown = false,  // указание на активное нажатие мыши (не используется для тач-событий)
-      startX = 0,           // начало координат по оси X (pageX)
-      distX = 0,            // дистанция, пройденная указателем по оси X
-      startY = 0,           // начало координат по оси Y (pageY)
-      distY = 0,            // дистанция, пройденная указателем по оси Y
-      startTime = 0,        // время начала касания
-      support = {           // поддерживаемые браузером типы событий
-        pointer: !!("PointerEvent" in window || ("msPointerEnabled" in window.navigator)),
-        touch: !!(typeof window.orientation !== "undefined" || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || "ontouchstart" in window || navigator.msMaxTouchPoints || "maxTouchPoints" in window.navigator > 1 || "msMaxTouchPoints" in window.navigator > 1)
+        if (el == $('.big-image-container')[0]) touchElement = 'bigImage';
+        if (el == $('.images-container')[0]) touchElement = 'imagesItem';
       };
 
-    /**
-     * Опредление доступных в браузере событий: pointer, touch и mouse.
-     * returns {Object} - возвращает объект с доступными событиями.
-     */
-    var getSupportedEvents = function () {
-      switch (true) {
-        case support.pointer:
-          events = {
-            type: "pointer",
-            start: "PointerDown",
-            move: "PointerMove",
-            end: "PointerUp",
-            cancel: "PointerCancel",
-            leave: "PointerLeave"
-          };
-          // добавление префиксов для IE10
-          var ie10 = (window.navigator.msPointerEnabled && Function('/*@cc_on return document.documentMode===10@*/')());
-          for (var value in events) {
-            if (value === "type") continue;
-            events[value] = (ie10) ? "MS" + events[value] : events[value].toLowerCase();
+      function checkMove(e) {
+        if (isMouse && !isMouseDown) return; // exit function if the mouse is no longer active while driving
+        let event = e.changedTouches[0];
+        distX = event.pageX - startX;
+        distY = event.pageY - startY;
+        if (Math.abs(distX) > Math.abs(distY)) dir = (distX < 0) ? "left" : "right";
+        else dir = (distY < 0) ? "up" : "down";
+        // e.preventDefault();
+
+        if (touchElement == 'bigImage') $('.big-image').css({ left: `calc(50% + ${distX / 4}px)` });
+        if (touchElement == 'imagesItem') $('.images-item').css({ left: distX / 4 + 'px' });
+      };
+
+      function checkEnd(e) {
+        let endTime = new Date().getTime();
+        let time = endTime - startTime;
+        if (time >= settings.minTime && time <= settings.maxTime) {
+          if (Math.abs(distX) >= settings.minDist && Math.abs(distY) <= settings.maxDist) {
+            swipeType = dir; // check character as "left" or "right"
+          } else if (Math.abs(distY) >= settings.minDist && Math.abs(distX) <= settings.maxDist) {
+            swipeType = dir; // check character as "top" or "down"
           }
-          break;
-        case support.touch:
-          events = {
-            type: "touch",
-            start: "touchstart",
-            move: "touchmove",
-            end: "touchend",
-            cancel: "touchcancel"
-          };
-          break;
-        default:
-          events = {
-            type: "mouse",
-            start: "mousedown",
-            move: "mousemove",
-            end: "mouseup",
-            leave: "mouseleave"
-          };
-          break;
-      }
-      return events;
-    };
-
-
-    /**
-     * Объединение событий mouse/pointer и touch.
-     * param e {Event} - принимает в качестве аргумента событие.
-     * returns {TouchList|Event} - возвращает либо TouchList, либо оставляет событие без изменения.
-     */
-    var eventsUnify = function (e) {
-      return e.changedTouches ? e.changedTouches[0] : e;
-    };
-
-
-    /**
-     * Обрабочик начала касания указателем.
-     * param e {Event} - получает событие.
-     */
-    var checkStart = function (e) {
-      var event = eventsUnify(e);
-      if (support.touch && typeof e.touches !== "undefined" && e.touches.length !== 1) return; // игнорирование касания несколькими пальцами
-      dir = "none";
-      swipeType = "none";
-      dist = 0;
-      startX = event.pageX;
-      startY = event.pageY;
-      startTime = new Date().getTime();
-      if (isMouse) isMouseDown = true; // поддержка мыши
-      e.preventDefault();
-    };
-
-    /**
-     * Обработчик движения указателя.
-     * param e {Event} - получает событие.
-     */
-    var checkMove = function (e) {
-      if (isMouse && !isMouseDown) return; // выход из функции, если мышь перестала быть активна во время движения
-      var event = eventsUnify(e);
-      distX = event.pageX - startX;
-      distY = event.pageY - startY;
-      if (Math.abs(distX) > Math.abs(distY)) dir = (distX < 0) ? "left" : "right";
-      else dir = (distY < 0) ? "up" : "down";
-      e.preventDefault();
-
-      $('.big-image').css({['left']: distX + 'px'});
-      console.log(distX);
-
-    };
-
-    /**
-     * Обработчик окончания касания указателем.
-     * param e {Event} - получает событие.
-     */
-    var checkEnd = function (e) {
-      var endTime = new Date().getTime();
-      var time = endTime - startTime;
-      if (time >= settings.minTime && time <= settings.maxTime) { // проверка времени жеста
-        if (Math.abs(distX) >= settings.minDist && Math.abs(distY) <= settings.maxDist) {
-          swipeType = dir; // опредление типа свайпа как "left" или "right"
-        } else if (Math.abs(distY) >= settings.minDist && Math.abs(distX) <= settings.maxDist) {
-          swipeType = dir; // опредление типа свайпа как "top" или "down"
         }
-      }
-      dist = (dir === "left" || dir === "right") ? Math.abs(distX) : Math.abs(distY); // опредление пройденной указателем дистанции
+        dist = (dir === "left" || dir === "right") ? Math.abs(distX) : Math.abs(distY); // check distance
+        // e.preventDefault();
 
-      // генерация кастомного события swipe
-      if (swipeType !== "none" && dist >= settings.minDist) {
-        var swipeEvent = new CustomEvent("swipe", {
-          bubbles: true,
-          cancelable: true,
-          detail: {
-            full: e, // полное событие Event
-            dir: swipeType, // направление свайпа
-            dist: dist, // дистанция свайпа
-            time: time // время, потраченное на свайп
-          }
-        });
-        el.dispatchEvent(swipeEvent);
-      }
-      e.preventDefault();
+        if (touchElement == 'bigImage' && swipeType == 'left') arrow('next');
+        if (touchElement == 'bigImage' && swipeType == 'right') arrow('prev');
 
-      console.log(swipeType);
+        if (touchElement == 'imagesItem' && swipeType == 'left') arrow('scrollNext');
+        if (touchElement == 'imagesItem' && swipeType == 'right') arrow('scrollPrev');
 
-      if (swipeType == 'left') arrow('next');
-      if (swipeType == 'right') arrow('prev');
+        if (isMouse && isMouseDown) { // exit function and reset 'mouse click' test
+          if (touchElement == 'bigImage') $('.big-image').css({ left: '50%' });
+          if (touchElement == 'imagesItem') $('.images-item').css({ left: 0 });
+          isMouseDown = false;
+          return;
+        }
+        if (touchElement == 'bigImage') $('.big-image').css({ left: '50%' });
+        if (touchElement == 'imagesItem') $('.images-item').css({ left: 0 });
+      };
 
-      if (isMouse && isMouseDown) { // выход из функции и сброс проверки нажатия мыши
-      $('.big-image').css({['left']: 0});
-        isMouseDown = false;
-        return;
-      }
+      // add supported events
+      let events = {
+        type: "touch",
+        start: "touchstart",
+        move: "touchmove",
+        end: "touchend",
+        cancel: "touchcancel",
+        leave: "mouseleave",
+      };
+
+      // add handlers to an item
+      el.addEventListener(events.start, checkStart);
+      el.addEventListener(events.move, checkMove);
+      el.addEventListener(events.end, checkEnd);
+
     };
 
-    // добавление поддерживаемых событий
-    var events = getSupportedEvents();
+    let bigImageContainer = $('.big-image-container')[0];
+    swipe(bigImageContainer);
+    bigImageContainer.addEventListener("swipe", () => { });
 
-    // проверка наличия мыши
-    if ((support.pointer && !support.touch) || events.type === "mouse") isMouse = true;
-
-    // добавление обработчиков на элемент
-    el.addEventListener(events.start, checkStart);
-    el.addEventListener(events.move, checkMove);
-    el.addEventListener(events.end, checkEnd);
-
-  };
-
-
-  // элемент
-  var myBlock = document.getElementsByClassName('big-image-container')[0];
-
-  // вызов функции swipe с предварительными настройками
-  swipe(myBlock, { maxTime: 2000, minTime: 10, maxDist: 200, minDist: 60 });
-
-  // обработка свайпов
-  myBlock.addEventListener("swipe", function () { });
+    let imagesContainer = $('.images-container')[0];
+    swipe(imagesContainer);
+    imagesContainer.addEventListener("swipe", () => { });
+  }
 });
